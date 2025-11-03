8a9e8e80f89991665b606b461dee2c7b9d9859df
PIUSOLOFU <piusolofu4@gmail.com>
2025-11-03 08:31:00 +0100

    Install deps for serverless Express + HBS

--- api/index.js
+++ api/index.js
@@ -1,9 +1,27 @@
 // api/index.js
-const serverless = require('serverless-http');
-const app = require('../app');
+import express from "express";
+import { engine as hbsEngine } from "express-handlebars";
+import serverless from "serverless-http";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// Wrap the Express app into a serverless handler
-module.exports = (req, res) => {
-  const handler = serverless(app);
-  return handler(req, res);
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// serve static files
+app.use(express.static(path.join(__dirname, "../public")));
+
+// handlebars
+app.engine("hbs", hbsEngine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../views"));
+
+// routes
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+// no app.listen on Vercel
+export default (req, res) => serverless(app)(req, res);
